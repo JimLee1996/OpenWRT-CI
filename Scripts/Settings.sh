5a3e6cad2db1870863a82bbc1de6fc1ab6ab9d85
JimLee1996 <59061463@qq.com>
2025-03-03 15:59:59 +0800

    feat: k2p

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -60,5 +60,66 @@ fi
 #编译器优化
 if [[ $WRT_TARGET != *"X86"* ]]; then
 	echo "CONFIG_TARGET_OPTIONS=y" >> ./.config
-	echo "CONFIG_TARGET_OPTIMIZATION=\"-O2 -pipe -march=armv8-a+crypto+crc -mcpu=cortex-a53+crypto+crc -mtune=cortex-a53\"" >> ./.config
+	if [[ $WRT_CONFIG != 'K2P' ]]; then
+		echo "CONFIG_TARGET_OPTIMIZATION=\"-O2 -pipe -march=armv8-a+crypto+crc -mcpu=cortex-a53+crypto+crc -mtune=cortex-a53\"" >> ./.config
+	fi
+fi
+
+if [[ $WRT_CONFIG == 'K2P' ]]; then
+	#网络设置
+	NET_SH="./package/base-files/files/etc/uci-defaults/991_set-network.sh"
+	if [ ! -f "$NET_SH" ]; then
+		cat << 'EOF' > $NET_SH
+#!/bin/sh
+
+# 检查 network.globals.ula_prefix 是否存在且不为空
+ula_prefix=$(uci get network.globals.ula_prefix 2>/dev/null)
+
+if [ -n "$ula_prefix" ]; then
+	uci set dhcp.wan6=dhcp
+	uci set dhcp.wan6.interface='wan6'
+	uci set dhcp.wan6.ignore='1'
+
+	uci set dhcp.lan.force='1'
+	uci set dhcp.lan.ra='hybrid'
+	uci set dhcp.lan.ra_default='1'
+	uci set dhcp.lan.max_preferred_lifetime='1800'
+	uci set dhcp.lan.max_valid_lifetime='3600'
+
+	uci del dhcp.lan.dhcpv6
+	uci del dhcp.lan.ra_flags
+	uci del dhcp.lan.ra_slaac
+	uci add_list dhcp.lan.ra_flags='none'
+
+	uci commit dhcp
+
+	uci set network.wan6.reqaddress='try'
+	uci set network.wan6.reqprefix='auto'
+	uci set network.lan.ip6assign='64'
+	uci set network.lan.ip6ifaceid='eui64'
+	uci set network.globals.packet_steering='0'
+	uci del network.globals.ula_prefix
+
+	uci commit network
+fi
+
+exit 0
+EOF
+		chmod +x $NET_SH
+	fi
+
+	#系统Reload
+	RELOAD_SH="./package/base-files/files/etc/uci-defaults/999_auto-restart.sh"
+	if [ ! -f "$RELOAD_SH" ]; then
+		cat << 'EOF' > $RELOAD_SH
+#!/bin/sh
+
+/etc/init.d/network restart
+/etc/init.d/odhcpd restart
+/etc/init.d/rpcd restart
+
+exit 0
+EOF
+		chmod +x $RELOAD_SH
+	fi
 fi
